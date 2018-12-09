37da1e77fc253168b5f192589650f3feeb2e5f3a
Skylar Salerno <SkylarSalerno@Skylars-MacBook-Air-25.local>
2018-12-08 22:06:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  switch(true){
+    case feet <= 400:
+      return 'This one is on me!'
+    case feet > 2500:
+      return 'No can do.'
+    case feet > 2000:
+      return 'I will gladly take your thirty bucks.'
+
+  }
+}
+function ternaryCheckCity(city){
+return (city==='NYC' ? "Ok, sounds good." :'No go.')
+}
+function switchOnCharmFromTip(tip){
+  if (tip==='generous'){ return 'Thank you so much.'}
+  else if (tip==='not as generous'){ return 'Thank you.'}
+  else {return 'Bye.'}
+  }
